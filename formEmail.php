f2ec88720cf4ac57bdce751b3e381ea927d5acd4
Gareth Ferguson <gareth.f@hotmail.co.uk>
2014-05-25 18:30:49 +0100

    New email on form submit

--- formEmail.php
+++ formEmail.php
@@ -4,16 +4,16 @@ $name = $_POST['person_name'];
 $email = $_POST['person_email'];
 $themessage = $_POST['person_enq'];
 
-$myemail = 'gareth.f@hotmail.co.uk';
+$myemail = 'gazmatron1@gmail.com';
 $subject = 'Website Enquiry';
 
-$message = "Name: " . $name . "<br/> Email: " . $email . "<br/><br/> message: " . $themessage;
+$message = "Name: " . $name . "<br/> Email: " . $email . "<br/><br/> Message: " . $themessage;
 
 $headers = "From: ".$email."\r\n"; 
 $headers .= "Reply-To: ".$email."\r\n"; 
 $headers .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
 
-mail($myemail, $subject, $message,$headers,"-f gareth.f@hotmail.co.uk") or die ("failed");
+mail($myemail,$subject,$message,$headers,"-f $email") or die ("failed");
 
 header('Location: http://www.garethferguson.co.uk/index.php?email=success');
 
