d8e77b4e1a35a19b57b2ddcc3fcd36fd04c20b6a
Gareth Ferguson <gareth.f@hotmail.co.uk>
2016-09-14 16:44:32 +0100

    No Grunt

--- grunt/aliases.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (grunt) {
-
-    grunt.registerTask('default', ["concurrent:dev"]);
-    
-    grunt.registerTask('build', ['copy','sass','concat', 'critical', 'ngAnnotate', 'uglify', 'jsonmin', 'cssmin']);
-        
-};
--- grunt/ngAnnotate.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    app: {
-        files: {
-            '<%= paths.src.js %>/build/angular_app.js': ['<%= paths.dest.js %>/build/angular_app.js']
-        }
-    }
-};
